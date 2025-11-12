7acc31a56cdec63aca0edd2f905fa8570ec47bd7
HendiP <pratamahendy111@gmail.com>
2025-11-12 14:50:38 +0700

    UTS

--- public/client.js
+++ public/client.js
@@ -1,5 +1,3 @@
-// public/client.js
-
 const loginForm = document.getElementById('loginForm');
 const usernameInput = document.getElementById('username');
 const passwordInput = document.getElementById('password');
